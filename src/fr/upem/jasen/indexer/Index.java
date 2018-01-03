e8307b1537d476a554a50dc0d71ce7ccde6ce5f5
Xellement <xellement.tmbk@gmail.com>
2018-01-03 16:46:11 +0100

    Index

--- src/fr/upem/jasen/indexer/Index.java
+++ src/fr/upem/jasen/indexer/Index.java
@@ -1,5 +1,21 @@
 package fr.upem.jasen.indexer;
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class Index {
 	
+	private ArrayList<Tokenizer> tokenList;
+	private String rep;
+	private String[] children;
+
+	public Index(String repPath) {
+		rep = repPath;
+		tokenList = new ArrayList<>();
+		children = new File(repPath).list();
+		for(int i=0;i<children.length;i++){
+			if(children[i].endsWith(".txt")) 
+				tokenList.add(new Tokenizer(rep+"\\"+children[i]));			
+		}
+	}	
 }
